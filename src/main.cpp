ec97fd70cbbc266aebaae4ade3e435867e292bf5
guillaume-haerinck <guillaume.haerinck@outlook.com>
2019-10-02 16:42:14 +0200

    Fix wasm build

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,16 @@
 	#include <crtdbg.h>
 #endif
 
-#include <entt/entt.hpp>
-
 void mainLoop(void* arg);
 
 struct MainLoopArg {
 	App* app;
 };
 
-struct test {
-	float x;
-};
-
-void freefonc(entt::entity entity, entt::registry & registry) {
-	auto component = registry.get<test>(entity);
-	component.x = 5;
-}
-
 int main(int argc, char *argv[]) {
 	#ifdef _WIN32 // Check memory leaks
 		_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	#endif
-
-	entt::registry registry;
-	auto entity = registry.create();
-	registry.on_destroy<test>().connect<&freefonc>();
-	test mytest = {};
-	registry.assign<test>(entity, mytest);
-	registry.remove<test>(entity);
 
 	App* app = new App();
 	MainLoopArg arg = {};
