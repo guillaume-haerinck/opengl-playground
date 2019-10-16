794f3aa1293b8d67a50411ad2be1d43f10b3a830
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-10-16 14:43:14 +0200

    Fix texture loading

--- src/graphics/render-command.cpp
+++ src/graphics/render-command.cpp
@@ -63,8 +63,8 @@ scomp::Texture RenderCommand::createTexture(unsigned int slot, const char* filep
 	// TODO remove and use sampler instead
 	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
 	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT));
+	GLCall(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT));
 
 	GLCall(glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, localBuffer));
 	GLCall(glBindTexture(GL_TEXTURE_2D, 0)); // unbind
